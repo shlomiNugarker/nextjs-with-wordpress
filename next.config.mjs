9c9225930fa20c36b91cac2ef67f3521f8570498
Shlomi Nugarker <shlomin1231@gmail.com>
2024-07-16 09:24:10 +0300

    theme

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,23 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {};
+import config from './src/app/config/config.json' assert { type: 'json' }
 
-export default nextConfig;
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  experimental: {
+    missingSuspenseWithCSRBailout: false,
+  },
+  reactStrictMode: true,
+  basePath: config.base_path !== '/' ? config.base_path : '',
+  trailingSlash: config.site.trailing_slash,
+  output: 'standalone',
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: '', // todo: add hostname
+        pathname: '/**',
+      },
+    ],
+  },
+}
+
+export default nextConfig

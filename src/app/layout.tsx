2c9ed0d3dc5901a80c2cbafbed6948c0c86c4d1f
Shlomi Nugarker <shlomin1231@gmail.com>
2024-07-16 09:03:08 +0300

    layout

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,7 +1,8 @@
 import type { Metadata } from 'next'
 import { Inter } from 'next/font/google'
-import './globals.css'
 import '../styles/main.scss'
+import theme from './config/theme.json'
+import config from './config/config.json'
 import Providers from '@/layouts/partials/Providers'
 
 const inter = Inter({ subsets: ['latin'] })
@@ -16,8 +17,47 @@ export default function RootLayout({
 }: Readonly<{
   children: React.ReactNode
 }>) {
+  // import google font css
+  const pf = theme.fonts.font_family.primary
+  const sf = theme.fonts.font_family.secondary
   return (
-    <html lang="en">
+    <html suppressHydrationWarning={true} lang="en">
+      <head>
+        {/* responsive meta */}
+        <meta
+          name="viewport"
+          content="width=device-width, initial-scale=1, maximum-scale=5"
+        />
+
+        {/* favicon */}
+        <link rel="shortcut icon" href={config.site.favicon} />
+        {/* theme meta */}
+        <meta name="theme-name" content="content" />
+        <meta name="msapplication-TileColor" content="#000000" />
+        <meta
+          name="theme-color"
+          media="(prefers-color-scheme: light)"
+          content="#fff"
+        />
+        <meta
+          name="theme-color"
+          media="(prefers-color-scheme: dark)"
+          content="#000"
+        />
+
+        {/* google font css */}
+        <link
+          rel="preconnect"
+          href="https://fonts.gstatic.com"
+          crossOrigin="anonymous"
+        />
+        <link
+          href={`https://fonts.googleapis.com/css2?family=${pf}${
+            sf ? '&family=' + sf : ''
+          }&display=swap`}
+          rel="stylesheet"
+        />
+      </head>
       <body className={inter.className}>
         <Providers>{children}</Providers>
       </body>

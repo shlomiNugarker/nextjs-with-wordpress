8e6e096d8ebc73271b52575f1a3f61d11a4249f9
Shlomi Nugarker <shlomin1231@gmail.com>
2024-07-16 08:52:34 +0300

    add saas

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from 'next'
 import { Inter } from 'next/font/google'
 import './globals.css'
+import '../styles/main.scss'
 import Providers from '@/layouts/partials/Providers'
 
 const inter = Inter({ subsets: ['latin'] })

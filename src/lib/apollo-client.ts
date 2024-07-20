1dfcbc398bb05647240b1a81468dd77e99171aa8
Shlomi Nugarker <shlomin1231@gmail.com>
2024-07-20 11:14:18 +0300

    code formatted and commented for clarity

--- src/lib/apollo-client.ts
+++ src/lib/apollo-client.ts
@@ -8,6 +8,7 @@ import { removeLastTrailingSlash } from './util'
 
 let client: ApolloClient<any> | undefined
 
+// Function to get the existing Apollo Client or create a new one
 export function getApolloClient(): ApolloClient<any> {
   if (!client) {
     client = _createApolloClient()
@@ -15,15 +16,16 @@ export function getApolloClient(): ApolloClient<any> {
   return client
 }
 
+// Function to create a new Apollo Client
 export function _createApolloClient(): ApolloClient<any> {
   const defaultOptions: DefaultOptions = {
     watchQuery: {
-      fetchPolicy: 'no-cache',
-      errorPolicy: 'ignore',
+      fetchPolicy: 'no-cache', // Do not cache watch queries
+      errorPolicy: 'ignore', // Ignore errors in watch queries
     },
     query: {
-      fetchPolicy: 'no-cache',
-      errorPolicy: 'all',
+      fetchPolicy: 'no-cache', // Do not cache queries
+      errorPolicy: 'all', // Report all errors in queries
     },
   }
 
@@ -33,7 +35,7 @@ export function _createApolloClient(): ApolloClient<any> {
         process.env.WORDPRESS_GRAPHQL_ENDPOINT as string
       ),
     }),
-    cache: new InMemoryCache(),
-    defaultOptions,
+    cache: new InMemoryCache(), // Use an in-memory cache for query results
+    defaultOptions, // Apply the default options to the client
   })
 }

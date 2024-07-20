46b2d2a746703fc9cf15fb49c7ecdb473cc7be91
Shlomi Nugarker <shlomin1231@gmail.com>
2024-07-20 12:58:27 +0300

    add fields in query posts

--- src/queries/posts.ts
+++ src/queries/posts.ts
@@ -7,8 +7,60 @@ export const GET_ALL_POSTS = gql`
         id
         title
         content
+        excerpt
         slug
         date
+        modified
+        status
+        author {
+          node {
+            id
+            name
+            slug
+            description
+          }
+        }
+        categories {
+          nodes {
+            id
+            name
+            slug
+          }
+        }
+        tags {
+          nodes {
+            id
+            name
+            slug
+          }
+        }
+        featuredImage {
+          node {
+            sourceUrl
+            title
+            description
+            altText
+            caption
+            sourceUrl
+            srcSet
+            sizes
+            id
+          }
+        }
+        comments {
+          nodes {
+            id
+            content
+            date
+            author {
+              node {
+                id
+                name
+                email
+              }
+            }
+          }
+        }
       }
     }
   }

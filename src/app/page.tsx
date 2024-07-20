7fc5cc59165caa06f957be3575520a79ca746136
Shlomi Nugarker <shlomin1231@gmail.com>
2024-07-20 17:42:25 +0300

    app/error.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,7 +2,6 @@ import SeoMeta from '@/layouts/partials/SeoMeta'
 import { getAllUsers } from '@/lib/users'
 
 export default async function Home() {
-  getAllUsers()
   return (
     <>
       <SeoMeta />

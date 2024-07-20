48d6cb3e1f45fc268285fa5889be0c9b86357548
Shlomi Nugarker <shlomin1231@gmail.com>
2024-07-20 12:59:15 +0300

    add interfaces for Post querys

--- src/types/Post.ts
+++ src/types/Post.ts
@@ -1,8 +1,63 @@
-export interface Post {
+interface Author {
+  node: {
+    id: string
+    name: string
+    slug: string
+    description: string | null
+    __typename: string
+  }
+  __typename: string
+}
+
+interface Category {
+  id: string
+  name: string
+  slug: string
+}
+
+interface Tag {
+  id: string
+  name: string
+  slug: string
+}
+
+interface Comment {
+  id: string
+  content: string
+  author: Author
+  date: string
+}
+
+interface Post {
   id: string
   title: string
   content: string
+  excerpt: string
   slug: string
   date: string
-  __typename?: string // Optional, depending on if you use GraphQL or similar
+  modified: string
+  status: string
+  author: { node: Author }
+  categories: { nodes: Category[]; __typename: string }
+  tags: { nodes: Tag[]; __typename: string }
+  featuredImage: { node: FeaturedImage; __typename: string } | null
+  comments: { nodes: Comment[]; __typename: string }
+  __typename: string
+}
+
+interface FeaturedImage {
+  sourceUrl: string
+  title: string
+  description: string | null
+  altText: string
+  caption: string | null
+  srcSet: string | null
+  sizes: string
+  id: string
+  __typename: string
+}
+
+interface RootQueryToPostsConnection {
+  posts: { nodes: Post[] }
+  __typename: string
 }
